8d96ade8cb1882289ab8a06060bfed65f3d186ef
Markus Weiland <mw@2h10.de>
2022-08-29 14:47:34 +0200

    Remove use of external type

--- src/git/model/content-entry-draft.ts
+++ src/git/model/content-entry-draft.ts
@@ -1,6 +1,5 @@
 import { ContentEntry } from './content-entry'
 import { ContentEntryMetadata } from './content-entry-metadata'
-import { Entry } from '../../persistence/persistence.service'
 
 export class ContentEntryDraft extends ContentEntry {
   readonly deletion: boolean
@@ -8,7 +7,7 @@ export class ContentEntryDraft extends ContentEntry {
   constructor(
     id: string,
     metadata: ContentEntryMetadata,
-    data: Entry,
+    data: Record<string, unknown>,
     deletion: boolean,
   ) {
     super(id, metadata, data)

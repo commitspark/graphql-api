ee3cd6449a5d7c84c303bb2f50f6ac5d15d2968c
Markus Weiland <mw@2h10.de>
2023-05-12 22:34:38 +0200

    Remove deprecated code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 ### Changed
 - Rename organization
+- Remove deprecated code
 
 ## [0.6.1] - 2023-05-07
 ### Changed
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,5 @@
 const path = require('node:path')
 module.exports = (options, webpack) => {
-  const lazyImports = [
-    '@nestjs/microservices/microservices-module',
-    '@nestjs/websockets/socket-module',
-    '@nestjs/platform-express',
-  ]
-
   return {
     ...options,
     devtool: 'source-map', // create source map for debugging
@@ -19,18 +13,6 @@ module.exports = (options, webpack) => {
     },
     plugins: [
       ...options.plugins,
-      new webpack.IgnorePlugin({
-        checkResource(resource) {
-          if (lazyImports.includes(resource)) {
-            try {
-              require.resolve(resource)
-            } catch (err) {
-              return true
-            }
-          }
-          return false
-        },
-      }),
     ],
     resolve: {
       // @see https://github.com/apollographql/apollo-server/issues/4983
